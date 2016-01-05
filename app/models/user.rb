be05e21481337d409bc4eddc329159a94537c556
Fiore Basile <fiore.basile@gmail.com>
2016-01-05 16:55:59 +0100

    add extra fields to the users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
       user.uid = auth['uid']
       if auth['info']
          user.name = auth['info']['name'] || ""
+         user.email = auth['info']['email'] || ""
+         user.first_name = auth['info']['first_name'] || ""
+         user.last_name = auth['info']['last_name'] || ""
+         user.avatar = auth['info']['avatar'] || ""
       end
     end
   end
